248f27251efaaa3acc3b7f3f125dfd247d96be60
andreaslowe <andrea.s.lowe@gmail.com>
2017-04-27 15:02:40 -0400

    Update UTR Project_Part2.py

--- UTR Project_Part2.py
+++ UTR Project_Part2.py
@@ -47,25 +47,39 @@ for column in upreg_control_up:
             upreg_control_motif_up += 1
 upreg_percent_up = 100*(upreg_control_motif_up/float(len(control_up.columns)))
             
-#plot sequence lengths
+#prepare to plot sequence lengths and percentage of A, T, C, and G
 seq_len = pd.DataFrame.from_items([("Up Regulated", Nucleotides_Up["Sequence Length"]), ("Down Regulated", Nucleotides_Down["Sequence Length"])])
-seq_len.plot(kind = "box") #copy into command line to see
-
-#Plot percentage of A, T, C, and G
 A_percent = pd.DataFrame.from_items([("Up Regulated", Nucleotides_Up["A Percent"]), ("Down Regulated", Nucleotides_Down["A Percent"])])
-A_percent.plot(kind = "box") 
-
 T_percent = pd.DataFrame.from_items([("Up Regulated", Nucleotides_Up["T Percent"]), ("Down Regulated", Nucleotides_Down["T Percent"])])
-T_percent.plot(kind = "box")
-
 C_percent = pd.DataFrame.from_items([("Up Regulated", Nucleotides_Up["C Percent"]), ("Down Regulated", Nucleotides_Down["C Percent"])])
-C_percent.plot(kind = "box")
-
 G_percent = pd.DataFrame.from_items([("Up Regulated", Nucleotides_Up["G Percent"]), ("Down Regulated", Nucleotides_Down["G Percent"])])
+
+#Code to plot parameters - copy into command line to see
+seq_len.plot(kind = "box")
+A_percent.plot(kind = "box") 
+T_percent.plot(kind = "box")
+C_percent.plot(kind = "box")
 G_percent.plot(kind = "box")
+#**figures must be explicitly closed using: matplotlib.pyplot.close("all")
 
 #create dataframe that compares motifs in percent of sequences Up vs Down
 MotifPercent = pd.DataFrame.from_items([("Motifs", MotifPercent_Up.ix[:,0]), ("Up Regulated", MotifPercent_Up.ix[:,1]), ("Down Regulated", MotifPercent_Down.ix[:,1])])
+
+#stats differences in motif counts
+import scipy.stats as stats
+counts_down = MotifCounts_Down.drop(0, axis=1)
+counts_down = counts_down.T
+counts_up = MotifCounts_Up.drop(0, axis=1)
+counts_up = counts_up.T
+
+p_value = []
+index = []
+for columns in counts_down:
+    t, p = stats.ttest_ind(counts_down[columns], counts_up[columns], nan_policy= "omit")
+    if p <= 0.05:
+        index.append(columns)
+        p_value.append(p)
+significant_motifs = MotifCounts_Down.iloc[index][0]
 
 #Print percentage of control and upregulated
 print "Percent control motifs (AATAAA or ATTAAA) in down-regulated genes: %f" %control_percent_down
@@ -80,19 +94,20 @@ print "Motifs in greater than 60 percent of down regulated:"
 print MotifPercent_Down[MotifPercent_Down[1] > 55]
 
 #standard t-test for sequence length, can repeat for other factors - should probably look into chi-squared test and correcting for multiple comparisons (Type I error), etc
-import scipy.stats as stats
-print "statistcal differences in sequence length:"
-print stats.ttest_rel(seq_len["Up Regulated"], seq_len["Down Regulated"], nan_policy= "omit")
+print "statistical differences in sequence length:"
+print stats.ttest_ind(seq_len["Up Regulated"], seq_len["Down Regulated"], nan_policy= "omit")
 
-print "statistcal differences in A_percent:"
-print stats.ttest_rel(A_percent["Up Regulated"], A_percent["Down Regulated"], nan_policy= "omit")
+print "statistical differences in A_percent:"
+print stats.ttest_ind(A_percent["Up Regulated"], A_percent["Down Regulated"], nan_policy= "omit")
 
-print "statistcal differences in T_percent:"
-print stats.ttest_rel(T_percent["Up Regulated"], T_percent["Down Regulated"], nan_policy= "omit")
+print "statistical differences in T_percent:"
+print stats.ttest_ind(T_percent["Up Regulated"], T_percent["Down Regulated"], nan_policy= "omit")
 
-print "statistcal differences in C_percent:"
-print stats.ttest_rel(C_percent["Up Regulated"], C_percent["Down Regulated"], nan_policy= "omit")
+print "statistical differences in C_percent:"
+print stats.ttest_ind(C_percent["Up Regulated"], C_percent["Down Regulated"], nan_policy= "omit")
 
-print "statistcal differences in G_percent:"
-print stats.ttest_rel(G_percent["Up Regulated"], G_percent["Down Regulated"], nan_policy= "omit")
+print "statistical differences in G_percent:"
+print stats.ttest_ind(G_percent["Up Regulated"], G_percent["Down Regulated"], nan_policy= "omit")
 
+print "There were %i motifs that had significant differencs in the counts between up and down-regulated genes." %len(index) 
+print "Type significant_motifs to see them and p_value to see associated values"
